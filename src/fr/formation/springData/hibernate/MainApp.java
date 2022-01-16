47f271b4916eaddd8a2ac066f78d3f1c2a58ef09
RAHMI <rachid.rahmi@atos.net>
2022-01-16 14:03:54 +0100

    add hibernate.cfg.xml file

--- src/fr/formation/springData/hibernate/MainApp.java
+++ src/fr/formation/springData/hibernate/MainApp.java
@@ -1,5 +1,13 @@
 package fr.formation.springData.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import fr.formation.springData.hibernate.model.Client;
+
+
+
+
 //import java.sql.Connection;
 //import java.sql.DriverManager;
 
@@ -20,6 +28,11 @@ public class MainApp {
 //			System.out.println(e.toString());		
 //		}
 		
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Client.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
 
 	}
 
---
+++ src/fr/formation/springData/hibernate/model/Client.java
@@ -0,0 +1,59 @@
+package fr.formation.springData.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="client")
+public class Client {
+	
+	@Id
+	@Column(name="client_id")
+	private Long id;
+	
+	@Column(name="client_name")
+	private String fullName;
+	
+	@Column(name="client_age")
+	private int age;
+	
+	@Column(name="client_address")
+	private String address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}

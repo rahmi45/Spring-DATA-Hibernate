9205ddb0e37e82cab05b12b52299d6ac4a5398b6
RAHMI <rachid.rahmi@atos.net>
2022-01-16 11:33:24 +0100

    Spring DATA Hibernate Testing

---
+++ src/fr/formation/springData/hibernate/MainApp.java
@@ -0,0 +1,26 @@
+package fr.formation.springData.hibernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/employee?allowPublicKeyRetrieval=true&useSSL=false";
+		String username = "rahmi";
+		String password = "rahmi";
+		
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected !");
+			System.out.println(connection);
+				
+		} catch (Exception e) {
+			System.out.println(e.toString());		
+		}
+		
+
+	}
+
+}
